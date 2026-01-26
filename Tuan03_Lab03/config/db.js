c05d34c5feb650995a1837408a67ae1da0e64585
urglydog <22667051.thien@student.iuh.edu.vn>
2026-01-26 10:16:55 +0700

    'lab03'

---
+++ Tuan03_Lab03/config/db.js
@@ -0,0 +1,12 @@
+const { DynamoDBClient } = require("@aws-sdk/client-dynamodb");
+require('dotenv').config();
+
+const client = new DynamoDBClient({
+  region: process.env.AWS_REGION,
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  },
+});
+
+module.exports = client;
